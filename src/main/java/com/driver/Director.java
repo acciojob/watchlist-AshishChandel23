95353569eeda38b2a69959b9342d1385d589fe53
Ashish Chandel <ashishchandel023@gmail.com>
2023-05-22 22:08:16 +0530

    third done

--- src/main/java/com/driver/Director.java
+++ src/main/java/com/driver/Director.java
@@ -9,14 +9,14 @@ import lombok.Setter;
 public class Director {
     private String name;
     private int numberOfMovies;
-    private double imbdRating;
+    private double imdbRating;
 
     public Director() {
     }
 
-    public Director(String name, int numberOfMovies, double imbdRating) {
+    public Director(String name, int numberOfMovies, double imdbRating) {
         this.name = name;
         this.numberOfMovies = numberOfMovies;
-        this.imbdRating = imbdRating;
+        this.imdbRating = imdbRating;
     }
 }
